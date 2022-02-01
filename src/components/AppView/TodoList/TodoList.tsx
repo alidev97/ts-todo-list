fa816a6f9eed3b4668c101363880754f24bb6d19
alidev97 <johnnywondering@gmail.com>
2022-02-01 15:21:23 +0300

    Удаление одного элемента

--- src/components/AppView/TodoList/TodoList.tsx
+++ src/components/AppView/TodoList/TodoList.tsx
@@ -1,34 +1,63 @@
 import React from 'react';
-import { List, Divider, Checkbox } from 'antd';
-import { DeleteFilled } from '@ant-design/icons';
+import {
+  List, Divider, Checkbox, Modal, Alert,
+} from 'antd';
+import { DeleteFilled, ExclamationCircleOutlined } from '@ant-design/icons';
 import styles from './TodoList.module.css';
-import { useAppSelector } from '../../../app/hooks';
-import { selectTodos } from '../../../features/todos/todos';
+import { useAppDispatch, useAppSelector } from '../../../app/hooks';
+import {
+  deleteSingleTodo,
+  ITodo,
+  selectTodos,
+} from '../../../features/todos/todos';
+
+const { confirm } = Modal;
 
 export const TodoList: React.FC = () => {
   const todos = useAppSelector(selectTodos);
+  const dispatch = useAppDispatch();
+
+  const handleItemDeletion = (todo: ITodo) => {
+    confirm({
+      title: 'Вы уверены, что хотите удалить данный элемент?',
+      icon: <ExclamationCircleOutlined />,
+      okType: 'danger',
+      onOk() {
+        dispatch(deleteSingleTodo(todo));
+      },
+    });
+  };
 
   return (
     <section className={styles.todoList}>
       <Divider orientation="left">Задачи</Divider>
-      <List
-        size="small"
-        bordered
-        className={styles.content}
-        dataSource={todos}
-        renderItem={(todo) => (
-          <List.Item
-            actions={[
-              <DeleteFilled
-                className={styles.deleteBtn}
-                onClick={() => console.log(todo)}
-              />,
-            ]}
-          >
-            <Checkbox>{todo.title}</Checkbox>
-          </List.Item>
-        )}
-      />
+      {todos.length > 0 ? (
+        <List
+          size="small"
+          bordered
+          className={styles.content}
+          dataSource={todos}
+          renderItem={(todo) => (
+            <List.Item
+              actions={[
+                <DeleteFilled
+                  className={styles.deleteBtn}
+                  onClick={() => handleItemDeletion(todo)}
+                />,
+              ]}
+            >
+              <Checkbox>{todo.title}</Checkbox>
+            </List.Item>
+          )}
+        />
+      ) : (
+        <Alert
+          message="Список задач пуст!"
+          description="Добавьте новую задачу."
+          type="info"
+          showIcon
+        />
+      )}
     </section>
   );
 };
--- src/features/todos/todos.ts
+++ src/features/todos/todos.ts
@@ -1,7 +1,7 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { RootState } from '../../app/store';
 
-interface ITodo {
+export interface ITodo {
   id: number;
   title: string;
 }
